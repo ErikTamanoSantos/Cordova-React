1df6de17172ea5d615cbfd0c034cb3e2c4e52db5
ErikTamano <72156568+ErikTamanoSantos@users.noreply.github.com>
2023-12-01 18:38:19 +0100

    first commit, not functional

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import registerServiceWorker from './serviceWorker';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
+
+const startApp = () => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  //ReactDOM.render(<App />, document.getElementById('root'));
+  //registerServiceWorker();
+};
+
+if(window.cordova) {
+  document.addEventListener('deviceready', startApp, false);
+} else {
+  startApp();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
